17f0aa6aaeff0cef5d756bc80ad581ee57a1eab8
„lapasaste“ <91331057+lapasaste@users.noreply.github.com>
2021-12-03 14:23:56 +0100

    add data in json-file

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask, request
 from flask import render_template
+import json
 
 app = Flask("Hello You")
 
 
 @app.route('/home')
 def hello():
+
     return render_template("index.html", name="Stefan")
 
 @app.route('/ueberblick')
@@ -14,13 +16,19 @@ def ueberblick():
 
 @app.route('/erfassen', methods=['GET', 'POST'])
 def erfassen():
+    d = open("datenspeicher.json")
+    datenspeicher_list = json.load(d)
 
     if request.method == 'POST':
-        modul= request.form.get("modul")
+        modul = request.form.get("modul")
         titletext = request.form.get("text_homework")
         notizen = request.form.get("textarea_notice")
         prioritaet = request.form.get("flexRadioDefault")
         faelligkeit = request.form.get("date")
+
+        datenspeicher_list.append({"faelligkeitdatum": faelligkeit, "modul": modul, "notizen": notizen, "prioritaet": prioritaet, "titelHausaufgabe": titletext})
+        with open("datenspeicher.json", "w") as f:
+            json.dump(datenspeicher_list, f, indent=4, separators=(",", ":"), sort_keys=True)
 
         return render_template("ueberblick.html", Modul=modul, Titel=titletext, Notizen=notizen, Priorität=prioritaet, Fälligkeit=faelligkeit)
 
